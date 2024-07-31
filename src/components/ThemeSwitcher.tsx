daa08afe7f4f5e64115e609117e4f8c97cfb81a5
Darshan Puttaswamy <darshan.px@oracle.com>
2024-07-31 09:22:16 +0530

    fixed issue with theme change

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,6 +1,6 @@
 //@ts-nocheck
 "use client"
-import {useTheme} from "next-themes";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { SunIcon } from './Icons/Sun';
 import { MoonIcon } from './Icons/Moon';
@@ -8,66 +8,69 @@ import React from 'react'
 
 
 export const useThemeSwitcher = () => {
-  const preferDarkQuery = "(prefer-color-schema: dark)";
-  const [mode, setMode] = useState("")
-  useEffect(()=>{
-    const mediaQuery = window.matchMedia(preferDarkQuery)
-    const userPref = window.localStorage.getItem("theme")
-    const handleChange = () => {
-        
-        if (userPref){
-            let check = userPref === 'dark' ?"dark": "light";
-            setMode(check)    
-            if(check == 'dark'){
-                document.documentElement.classList.add("dark")
-            }else{
-                document.documentElement.classList.remove("dark")
+    const preferDarkQuery = "(prefer-color-schema: dark)";
+    const userPref = window.localStorage.getItem("theme") || ""
+    const [mode, setMode] = useState(userPref)
+    useEffect(() => {
+        const mediaQuery = window.matchMedia(preferDarkQuery)
+        const userPref = window.localStorage.getItem("theme")
+        console.log(userPref)
+        const handleChange = () => {
+
+            if (userPref) {
+                let check = userPref === 'dark' ? "dark" : "light";
+                setMode(check)
+                if (check == 'dark') {
+                    document.documentElement.classList.add("dark")
+                } else {
+                    document.documentElement.classList.remove("dark")
+                }
+            } else {
+                let check = mediaQuery.matches ? "dark" : "light";
+                setMode(check)
+                if (check == 'dark') {
+                    document.documentElement.classList.add("dark")
+                } else {
+                    document.documentElement.classList.remove("dark")
+                }
             }
-        }else{
-            let check = mediaQuery.matches ? "dark": "light";
-            setMode(check)    
-            if(check == 'dark'){
-                document.documentElement.classList.add("dark")
-            }else{
-                document.documentElement.classList.remove("dark")
-            }
+
         }
-        
-    }
 
-    mediaQuery.addEventListener("change", handleChange)
+        mediaQuery.addEventListener("change", handleChange)
 
-    return () => mediaQuery.removeEventListener("change",handleChange)
+        return () => mediaQuery.removeEventListener("change", handleChange)
 
-  },[])
+    }, [])
 
 
-  useEffect(()=>{
-    if(mode == 'dark'){
-        window.localStorage.setItem("theme", 'dark')
-        document.documentElement.classList.add('dark')
-    }else{
-        window.localStorage.setItem("theme", 'light')
-        document.documentElement.classList.remove('dark')
-    }
+    useEffect(() => {
+        if (mode == 'dark') {
+            window.localStorage.setItem("theme", 'dark')
+            document.documentElement.classList.add('dark')
+        } else {
+            window.localStorage.setItem("theme", 'light')
+            document.documentElement.classList.remove('dark')
+        }
 
-  },[mode])
+    }, [mode])
 
-  return [mode,setMode]
+    return [mode, setMode]
 };
 
 
-export default function ThemeSwitch({className}:{
-    className?:string
+export default function ThemeSwitch({ className }: {
+    className?: string
 }) {
-    let [mode,setMode] =  useThemeSwitcher()
-  return (
-    <button  onClick={(evt)=> {
-        evt.stopPropagation()
-        setMode(mode ==="light"?"dark":"light")} } className={`${className} ml-3 flex items-center justify-center rounded-full p-1 ${mode == "light" ? "bg-dark text-light":"bg-light text-dark"}`}>
-    {mode === 'dark'? <SunIcon className="fill-dark"/>: <MoonIcon className="fill-dark"/>}
-    </button>
-  )
+    let [mode, setMode] = useThemeSwitcher()
+    return (
+        <button onClick={(evt) => {
+            evt.stopPropagation()
+            setMode(mode === "light" ? "dark" : "light")
+        }} className={`${className} ml-3 flex items-center justify-center rounded-full p-1 ${mode == "light" ? "bg-dark text-light" : "bg-light text-dark"}`}>
+            {mode === 'dark' ? <SunIcon className="fill-dark" /> : <MoonIcon className="fill-dark" />}
+        </button>
+    )
 }
 
 
