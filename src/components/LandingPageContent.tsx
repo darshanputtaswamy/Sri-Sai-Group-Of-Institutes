dba720eba981402de168aa9fc3daa49909ac423a
Darshan Puttaswamy <darshan.px@oracle.com>
2024-07-31 09:00:39 +0530

    fixed issue with join us button

--- src/components/LandingPageContent.tsx
+++ src/components/LandingPageContent.tsx
@@ -4,7 +4,8 @@ import { motion } from "framer-motion";
 import React from "react";
 import { AuroraBackground } from "./aceternityui/aurora-background";
 import VisionMission from "./VisionMission";
- 
+import Link from 'next/link'
+
 export function LandingPageContent() {
   return (
     <>
@@ -25,9 +26,9 @@ export function LandingPageContent() {
         <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
         Celebrating Our Culture & Heritage
         </div>
-        <a href="/join-us"><button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+        <Link href="/join-us"><button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
           Join Us
-        </button></a>
+        </button></Link>
       </motion.div>
     </AuroraBackground>
    
