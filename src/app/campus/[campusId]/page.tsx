5bc628105a848302b0f880e234e15625395ea3ed
Darshan Puttaswamy <darshan.px@oracle.com>
2024-09-03 15:56:44 +0530

    campus details

--- src/app/campus/[campusId]/page.tsx
+++ src/app/campus/[campusId]/page.tsx
@@ -4,19 +4,58 @@ import { StarsBackground } from "@/components/aceternityui/stars-background";
 import { ThreeDCardDemo } from "@/components/TestCard";
 import TransitionEffect from "@/components/TransitionEffect";
 import Image from "next/image";
+import { campus_details } from "../../../data/required_data"
+import { ParallaxScroll } from '@/components/aceternityui/parallex-loader';
+
 
 export const dynamicParams = true;
 export async function generateStaticParams() {
-  return [{ campusId: "1" }, { campusId: "2"}];
+  return [{ campusId: "campus-narayanapura" }, { campusId: "campus-vijipura" }];
 }
 
-export default function CampusDetails({params}:{ params :{ campusId :string}}) {
 
+
+const CampusDetails = ({ title, description, imageLocation }: any) => {
+  return (
+    <div className="mt-6">
+      <h2 className="text-4xl font-bold">{title}</h2>
+      <p className="mt-2">{description}</p>
+    </div>
+  );
+};
+
+
+const CampusPhotos = ({ photos }:any) => {
+  return (
+    <div className="mt-6">
+      <h3 className="text-2xl font-semibold">Campus Photos</h3>
+      <ParallaxScroll images={photos} />;
+    </div>
+  );
+};
+
+const CoursesOffered = ({ courses }: any) => {
+  return (
+    <div className="mt-6">
+      <h3 className="text-2xl font-semibold">Courses Offered</h3>
+      <ul className="list-disc mt-2">
+        {courses.map((course: any, index: any) => (
+          <li key={index}>{course}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
+export default function page({ params }: { params: { campusId: string } }) {
+  const campus = campus_details.find((campus: any) => campus.id === params.campusId);
 
   return (
-    <div>{params.campusId}</div>
-
-
-  )
+    <div className="mx-auto py-10 px-20">
+      <CampusDetails title={campus.title} description={campus.description} imageLocation={campus.image_location} />
+      <CoursesOffered courses={campus.courses_offered} />
+      <CampusPhotos photos={campus.campus_photos} />
+    </div>
+  );
 }
- 
--- src/data/required_data.tsx
+++ src/data/required_data.tsx
@@ -1,32 +1,52 @@
-export let campus_details:any = [
+export let campus_details: any = [
     {
-      "title": "Narayanapura Campus",
-      "description": "Located in the serene environment of Narayanapura, this campus offers Pre-University Courses (PUC) with state-of-the-art facilities and experienced faculty, providing a strong foundation for students' academic and professional careers.",
-      "image_location": "/images/narayanapura-campus.avif",
-      "id": "campus-narayanapura",
-      "courses_offered": [
-        "Pre-University Course (PUC)"
-      ],
-      "campus_photos": [
-        "/images/narayanapura-photo1.avif",
-        "/images/narayanapura-photo2.avif",
-        "/images/narayanapura-photo3.avif"
-      ]
+        "title": "Narayanapura Campus",
+        "description": "Located in the serene environment of Narayanapura, this campus offers Pre-University Courses (PUC) with state-of-the-art facilities and experienced faculty, providing a strong foundation for students' academic and professional careers.",
+        "image_location": "https://images.unsplash.com/photo-1665491961340-ee9c11ee1990?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+        "id": "campus-narayanapura",
+        "courses_offered": [
+            "Pre-University Course (PUC)"
+        ],
+        "campus_photos": [
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+            "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+            "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+            "https://images.unsplash.com/photo-1682686581854-5e71f58e7e3f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+            "https://images.unsplash.com/photo-1510784722466-f2aa9c52fff6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+            "https://images.unsplash.com/photo-1505765050516-f72dcac9c60e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+            "https://images.unsplash.com/photo-1439853949127-fa647821eba0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2640&q=80",
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+            "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80"
+
+        ]
     },
     {
-      "title": "Vijipura Campus",
-      "description": "The Vijipura Campus is dedicated to advanced education, offering Law, Paramedical, and Postgraduate courses. With a focus on professional development and practical learning, this campus is equipped with modern labs and resources to support specialized education.",
-      "image_location": "/images/vijipura-campus.avif",
-      "id": "campus-vijipura",
-      "courses_offered": [
-        "Law",
-        "Paramedical",
-        "Postgraduate Courses (M.A, M.Com, M.Phil)"
-      ],
-      "campus_photos": [
-        "/images/vijipura-photo1.avif",
-        "/images/vijipura-photo2.avif",
-        "/images/vijipura-photo3.avif"
-      ]
+        "title": "Vijipura Campus",
+        "description": "The Vijipura Campus is dedicated to advanced education, offering Law, Paramedical, and Postgraduate courses. With a focus on professional development and practical learning, this campus is equipped with modern labs and resources to support specialized education.",
+        "image_location": "https://images.unsplash.com/photo-1705810591530-02053b1a9954?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+        "id": "campus-vijipura",
+        "courses_offered": [
+            "Law",
+            "Paramedical",
+            "Postgraduate Courses (M.A, M.Com, M.Phil)"
+        ],
+        "campus_photos": [
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+            "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
+            "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+            "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+            "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80"
+        ]
     }
-  ]
+]
