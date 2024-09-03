a68fdfe5ad08f68cba6da036478e3fadf46c9d07
Darshan Puttaswamy <darshan.px@oracle.com>
2024-09-03 14:52:42 +0530

    fix for build issue

--- src/app/campus/[campusId]/page.tsx
+++ src/app/campus/[campusId]/page.tsx
@@ -5,9 +5,18 @@ import { ThreeDCardDemo } from "@/components/TestCard";
 import TransitionEffect from "@/components/TransitionEffect";
 import Image from "next/image";
 
+export const dynamicParams = true;
+export async function generateStaticParams() {
+  return [{ campusId: "1" }, { campusId: "2"}];
+}
+
 export default function CampusDetails({params}:{ params :{ campusId :string}}) {
+
+
   return (
     <div>{params.campusId}</div>
+
+
   )
 }
  
