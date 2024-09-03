2e31f09276d69145a016adfc75827bdde373dc73
Darshan Puttaswamy <darshan.px@oracle.com>
2024-09-03 09:41:57 +0530

    president's message

--- src/components/PresidentsMessage.tsx
+++ src/components/PresidentsMessage.tsx
@@ -9,15 +9,15 @@ import bm from "../../public/bm.jpg"
 export function PresidentsMessage() {
     return (
         <div>
-            <BackgroundGradient className="rounded-[22px] h-[25rem] flex  p-4 sm:p-10 bg-white dark:bg-zinc-900">
+            <BackgroundGradient className="rounded-[22px] h-[40rem] md:h-[25rem] flex flex-col-reverse md:flex-row p-4 sm:p-10 bg-white dark:bg-zinc-900">
        
-                <div className="p-4 mt-6">
-                    <p className="text-base sm:text-xl text-black mt-20 mb-2 dark:text-neutral-200">
+                <div className="md:p-4 md:mt-6">
+                    <p className="text-base sm:text-xl text-black md:mt-20 md:mb-2 dark:text-neutral-200">
                         Dr. B Manjunath, President
                     </p>
 
                     <p className="text-sm text-neutral-600 dark:text-neutral-400">
-                        Dr. B Manjunath, the esteemed President of Sri Sai Group of Institutes, is a distinguished lawyer and senior advocate at the High Court of Karnataka. His unwavering commitment to promoting equality and empowering the youth of our country is evident through the numerous scholarships he has awarded to talented students. Dr. Manjunath's dedication and vision have been instrumental in the success of our institution.
+                        Dr. B Manjunath, the esteemed President of Sri Sai Group of Institutes, is a distinguished lawyer and senior advocate at the High Court of Karnataka. His unwavering commitment to promoting equality and empowering the youth of our country is evident through the numerous scholarships he has awarded to talented students. Dr. Manjunath&apos;s dedication and vision have been instrumental in the success of our institution.
                     </p>
                 </div>
                 <div className="min-w-72">
