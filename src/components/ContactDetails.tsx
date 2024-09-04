ba0990c6b6e37a1e3be430e71c6b5a3a6681a8dc
Darshan Puttaswamy <darshan.px@oracle.com>
2024-09-04 09:07:56 +0530

    fixed address

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -26,7 +26,7 @@ const ContactCard: React.FC<ContactDetails> = ({
         <div className='p-6 rounded-lg shadow-lg mb-8 mx-auto dark:bg-slate-800 dark:text-white bg-white text-black 
                  transition-transform transform hover:-translate-y-2 hover:scale-105 hover:shadow-2xl md:w-3/4 w-full'  >
             <h3 className="text-2xl font-semibold mb-4">{branchName}</h3>
-            <div className='flex'>
+            <div className='flex justify-between'>
                 <div className='contact details flex flex-col '>
                     <div className="flex items-start pt-2">
                         <div className='h-20 min-w-16 pl-5'>
@@ -62,7 +62,9 @@ const ContactCard: React.FC<ContactDetails> = ({
                     </div>
                 </div>
                 <div>
-
+                    <div id="contact-form" className="max-w-md w-full  rounded-2xl shadow-input bg-light dark:bg-dark">
+                        <iframe width="100%" height="300" src="https://maps.google.com/maps?width=100%&amp;height=300&amp;hl=en&amp;coord=52.70967533219885, -8.020019531250002&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" scrolling="no" ></iframe><br />
+                    </div >
 
                 </div>
             </div>
